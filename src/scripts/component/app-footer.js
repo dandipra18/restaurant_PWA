27aaf055afb8c9fe7d31e444e86e3c980bf18c7b
Dandi Prasetyo <dandiprasetyoo18.com>
2024-05-12 11:45:50 +0700

    initial commit

--- src/scripts/component/app-footer.js
+++ src/scripts/component/app-footer.js
@@ -7,7 +7,7 @@ class AppFooter extends HTMLElement {
     this.innerHTML = `
       <p>
         &copy; Copyright 2022 &dash; <a href="/">Resto Finder</a> | 
-        <a href="https://github.com/virgiawankusuma/restaurant-apps" target="_blank" >View on Github</a>
+        <a href="https://github.com/dandipra18/restaurant_PWA" target="_blank" >View on Github</a>
       </p>
       `;
   }
